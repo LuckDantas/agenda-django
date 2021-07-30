db08f8c85bda8893e336ca3686b274a0b94fec7f
Lucas Dantas <lucasicaro11@gmail.com>
2021-07-30 18:28:54 -0300

    Criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#Criando o banco de dados
+class Evento(models.Model):
+        titulo = models.CharField(max_length=100, null=True)
+        descricao = models.TextField(blank=True, null=True)
+        data_evento = models.DateTimeField(verbose_name="DATA DO EVENTO")
+        data_criacao = models.DateTimeField(auto_now=True)
+        usuario = models.ForeignKey(User, on_delete=models.CASCADE) #SE O USUÁRIO FOR EXCLUIDO EXCLUIR TUDO QUE É DEPENDENCIA DELE;
+
+        #Caso você tenha um banco de dados que necessite que a tabela tenha um nome específico
+        class Meta:
+                db_table="evento"
+
+        #Sempre que chamar o objeto retorna o título.
+        def __str__(self):
+                return self.titulo
+
+        def get_data_evento(self):
+                return self.data_evento.strftime('%d/%m/%Y')
