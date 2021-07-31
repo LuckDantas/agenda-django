6f2eaa3080fb135edd96ef0d003e3f7401d32f32
Lucas Dantas <lucasicaro11@gmail.com>
2021-07-31 17:09:03 -0300

    Incluido template para inserção de eventos da agenda. Incluido o gitignore para ignorar os arquivos compilados.

--- core/views.py
+++ core/views.py
@@ -41,3 +41,19 @@ def submit_login(request):
 def logout_user(request):
     logout(request)
     return redirect('/')
+
+@login_required(login_url='/login/') #requer que seja no elemento
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/') #requer que seja no elemento
+def submit_evento(request):
+    if request.POST: #apenas aceitar métodos POST (evitar invasão ou inserção de código)
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        #SALVAR NO BANCO DE DADOS (CREATE)
+        Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+
+    return redirect('/')
