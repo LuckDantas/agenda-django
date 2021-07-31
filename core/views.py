83154dad01c37437a1c39893a469e014dd38ec6a
Lucas Dantas <lucasicaro11@gmail.com>
2021-07-31 16:36:08 -0300

    Incluido função e tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
 from core.models import Evento
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.messages import error
+
 
 # Create your views here.
 
 #def index(request):
 #    return redirect('/agenda')
 
+#Se o usuário não estiver autenticado não conseguirá acessar. NECESSÁRIO LOGIN
+ #Necessário redirecionar para a página de login
+def login_user(request):
+    return render(request, 'login.html')
+
+@login_required(login_url='/login/') #requer que seja no elemento
 def lista_eventos(request):
-    #usuario = request.user  #necessário estar autenticado
-    evento = Evento.objects.all() #Evento.objects.filter(usuario=usuario)
+    usuario = request.user  #necessário estar autenticado para filtrar por usuário.
+    evento = Evento.objects.filter(usuario=usuario) #Evento.objects.all()
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+#authenticação
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+
+        else:
+            messages.error(request, "Usuário ou senha inválido.")
+    return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
